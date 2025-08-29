839bf6162f457532e1dd0cdb3caee2f4cad2dfcf
sat33shgit <sateesh_b2000@yahoo.com>
2025-08-28 21:48:39 -0700

    Update Flask app to use 0.0.0.0 and PORT env for Render deployment

--- app.py
+++ app.py
@@ -119,4 +119,6 @@ def download(filename):
     return send_file(os.path.join('static', filename), as_attachment=True)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get('PORT', 10000))
+    app.run(host='0.0.0.0', port=port, debug=True)
